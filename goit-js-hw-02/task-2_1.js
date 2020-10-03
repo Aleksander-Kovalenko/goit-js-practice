6df263ed923c1ba003dcb94e1f9bdbcb9bfed374
Aleksander-Kovalenko <coldfellow@gmail.com>
2020-10-03 11:43:52 +0300

    task-js-2_1

--- goit-js-hw-02/task-2_1.js
+++ goit-js-hw-02/task-2_1.js
@@ -3,7 +3,7 @@ const getItemsString = function (array) {
   // Write code under this line
   let result = '';
   for (let i = 0; i < array.length; i += 1) {
-    result += `${i + 1} - ${array[i]}`;
+    result += `${i + 1} - ${array[i]}\n`;
   }
   return result;
 };
